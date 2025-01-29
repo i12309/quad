5216d0e87838fe5576db37245c3c2fbfa60a9d11
i12309 <87040089+i12309@users.noreply.github.com>
2025-01-30 00:14:59 +0300

    1

--- game/PingPong.js
+++ game/PingPong.js
@@ -80,13 +80,14 @@ export class PingPong extends BaseModule {
         for (let i = 0; i < this.platform.width; i++) {
             const oldPlatformKey = `${this.offsetX + this.platform.x + i},${this.offsetY + this.fieldHeight - 5}`;
             //console.log(this.gridManager.selectedTiles[oldPlatformKey]);
-            delete this.gridManager.selectedTiles[oldPlatformKey];
+            //delete this.gridManager.selectedTiles[oldPlatformKey];
+            this.gridManager.selectedTiles[platformKey] = { type: 'none', color: this.gridManager.backgroundColor };
             //console.log(this.gridManager.selectedTiles[oldPlatformKey]);
         }
 
         for (let i = 0; i < this.platform.width; i++) {
             const platformKey = `${this.offsetX + this.platform.x + i},${this.offsetY + this.fieldHeight - 5}`;
-            //this.gridManager.selectedTiles[platformKey] = { type: 'platform', color: '#0000FF' };
+            this.gridManager.selectedTiles[platformKey] = { type: 'platform', color: '#0000FF' };
         }
 
         this.gridManager.updateVisibleTiles();
