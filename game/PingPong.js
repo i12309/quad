990e3ed515b0e44ac3dc0071d01a76b90f1fc8fa
i12309 <87040089+i12309@users.noreply.github.com>
2025-01-29 22:41:00 +0300

    1

--- game/PingPong.js
+++ game/PingPong.js
@@ -20,7 +20,6 @@ export class PingPong extends BaseModule {
 
     setup() {
         this.clear(); // Инициализируем игру
-        alert('!');
     }
 
     start() {
@@ -41,6 +40,7 @@ export class PingPong extends BaseModule {
         this.pause();
         this.ball = { x: Math.floor(this.fieldWidth / 2), y: Math.floor(this.fieldHeight / 2), dx: 1, dy: -1 };
         this.platform = { x: Math.floor(this.fieldWidth / 2) - Math.floor(this.platform.width / 2), width: 6 };
+        alert(this.ball);alert(this.platform);
         this.score = 0;
         this.gridManager.selectedTiles = {};
         this.calculateFieldDimensions(); // Рассчитываем размеры поля
