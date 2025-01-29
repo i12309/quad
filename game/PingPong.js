cb319c1cb1cbd9a7b09650f589ef2906ed99954a
i12309 <87040089+i12309@users.noreply.github.com>
2025-01-30 00:12:45 +0300

    1

--- game/PingPong.js
+++ game/PingPong.js
@@ -79,12 +79,13 @@ export class PingPong extends BaseModule {
             const oldPlatformKey = `${this.offsetX + this.platform.x + i},${this.offsetY + this.fieldHeight - 5}`;
             //console.log(this.gridManager.selectedTiles[oldPlatformKey]);
             delete this.gridManager.selectedTiles[oldPlatformKey];
+            this.gridManager.updateVisibleTiles();
             //console.log(this.gridManager.selectedTiles[oldPlatformKey]);
         }
 
         for (let i = 0; i < this.platform.width; i++) {
             const platformKey = `${this.offsetX + this.platform.x + i},${this.offsetY + this.fieldHeight - 5}`;
-            //this.gridManager.selectedTiles[platformKey] = { type: 'platform', color: '#0000FF' };
+            this.gridManager.selectedTiles[platformKey] = { type: 'platform', color: '#0000FF' };
         }
 
         this.gridManager.updateVisibleTiles();
