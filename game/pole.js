c3c28caeb05bb2f2aa9f5cdaf0824265e0a067de
i12309 <87040089+i12309@users.noreply.github.com>
2025-02-22 00:00:47 +0300

    '#39D353'

--- game/pole.js
+++ game/pole.js
@@ -66,7 +66,7 @@ export class pole extends BaseModule {
             for (let y = 1; y < this.fieldHeight + 1; y++) {
                 const key = `${x},${y}`;
                 const _color = Math.floor(Math.random() * this.colors.length);
-                this.gridManager.selectedTiles[key] = { type: 'wall', color: _color };
+                this.gridManager.selectedTiles[key] = { type: 'wall', color: '#39D353'}; //_color };
             }
         }
     }
